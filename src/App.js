dfddafa0b26620f32c74652dbc670e39ffbf936d
alexanderdev <devcrowmaster@gmail.com>
2022-02-04 10:46:03 -0500

    fix route

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <CountriesContextProvider>
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route path="/" element={<Home />} />
+              <Route index element={<Home />} />
               <Route path="country/:name" element={<Country />} />
             </Route>
           </Routes>
